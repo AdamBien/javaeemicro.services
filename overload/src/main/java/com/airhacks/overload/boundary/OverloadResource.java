e82244f766ff98844318f15b6b1cd33968d75e78
Adam Bien <abien@adam-bien.com>
2016-02-05 13:32:37 +0100

    Timeout handler implemented

--- overload/src/main/java/com/airhacks/overload/boundary/OverloadResource.java
+++ overload/src/main/java/com/airhacks/overload/boundary/OverloadResource.java
@@ -3,12 +3,15 @@ package com.airhacks.overload.boundary;
 import com.airhacks.porcupine.execution.boundary.Dedicated;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 /**
  *
@@ -27,8 +30,16 @@ public class OverloadResource {
 
     @GET
     public void answers(@Suspended AsyncResponse response) {
+        response.setTimeout(2, TimeUnit.SECONDS);
+        response.setTimeoutHandler(this::onTimeout);
         supplyAsync(finder::find, answers).
                 thenAccept(response::resume);
+    }
+
+    public void onTimeout(AsyncResponse asyncResponse) {
+        Response response = Response.status(Status.SERVICE_UNAVAILABLE).
+                header("reason", "overloaded").build();
+        asyncResponse.resume(response);
     }
 
 }
