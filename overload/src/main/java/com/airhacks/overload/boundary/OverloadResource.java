4b1c5e1c96f7886862fee3ad67ffcb35b2c4b278
Adam Bien <abien@adam-bien.com>
2016-02-15 11:27:46 +0100

    Reverted to blocking processing

--- overload/src/main/java/com/airhacks/overload/boundary/OverloadResource.java
+++ overload/src/main/java/com/airhacks/overload/boundary/OverloadResource.java
@@ -1,15 +1,12 @@
 package com.airhacks.overload.boundary;
 
 import com.airhacks.porcupine.execution.boundary.Dedicated;
-import static java.util.concurrent.CompletableFuture.supplyAsync;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -29,11 +26,8 @@ public class OverloadResource {
     ExecutorService answers;
 
     @GET
-    public void answers(@Suspended AsyncResponse response) {
-        response.setTimeout(2, TimeUnit.SECONDS);
-        response.setTimeoutHandler(this::onTimeout);
-        supplyAsync(finder::find, answers).
-                thenAccept(response::resume);
+    public String answers() {
+        return finder.find();
     }
 
     public void onTimeout(AsyncResponse asyncResponse) {
