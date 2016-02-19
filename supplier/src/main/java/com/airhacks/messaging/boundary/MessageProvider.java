35d647a310fff8bbd863c14dce87b4543cbc0c72
Adam Bien <abien@adam-bien.com>
2016-02-19 11:13:39 +0100

    Introducing circuit breaker (https://github.com/AdamBien/breakr/)

--- supplier/src/main/java/com/airhacks/messaging/boundary/MessageProvider.java
+++ supplier/src/main/java/com/airhacks/messaging/boundary/MessageProvider.java
@@ -1,7 +1,7 @@
 package com.airhacks.messaging.boundary;
 
+import com.airhacks.messaging.control.MessageFactory;
 import javax.ejb.Stateless;
-import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 
@@ -14,20 +14,10 @@ import javax.interceptor.Interceptors;
 public class MessageProvider {
 
     @Inject
-    Event<String> monitoringChannel;
+    MessageFactory factory;
 
     public String nextMessage() {
-        long timestamp = System.currentTimeMillis();
-        if (timestamp % 2 == 0) {
-            throw new IllegalStateException("Invalid timestamp! " + timestamp);
-        }
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-        }
-        String message = "hey duke ";
-        monitoringChannel.fire(message);
-        return message + timestamp;
+        return factory.nextMessage();
     }
 
 }
---
+++ supplier/src/main/java/com/airhacks/messaging/control/MessageFactory.java
@@ -0,0 +1,36 @@
+package com.airhacks.messaging.control;
+
+import com.airhacks.breakr.Breakr;
+import com.airhacks.breakr.IgnoreCallsWhen;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+import javax.interceptor.Interceptors;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@ApplicationScoped
+@Interceptors(Breakr.class)
+public class MessageFactory {
+
+    @Inject
+    Event<String> monitoringChannel;
+
+    @IgnoreCallsWhen(failures = 2)
+    public String nextMessage() {
+        long timestamp = System.currentTimeMillis();
+        if (timestamp % 2 == 0) {
+            throw new IllegalStateException("Invalid timestamp! " + timestamp);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+        }
+        String message = "hey duke ";
+        monitoringChannel.fire(message);
+        return message + timestamp;
+    }
+
+}
